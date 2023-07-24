8fad9f7b00655b6de967e0c4a2cbea7354d39df0
Udit Sharma <udit.sharma@dataverze.ai>
2023-07-24 15:30:33 +0530

    Created an endpoint /password
    Required files for the endpoints is added

--- server/endpoints.py
+++ server/endpoints.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pkgUdit import generate_pass
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -9,12 +10,15 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write(b'<h1>Pong!</h1>')
 
         elif self.path == '/password':
+            password = generate_pass()
             self.send_response(200)
-            self.send_header('content-type','text/html')
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b'<h1>Sample password for now</h1>')
+            self.wfile.write(f'<h1>Password: {password}</h1>'.encode())
+
         else:
             self.send_response(404)
             self.send_header('content-type','text/html')
             self.end_headers()
             self.wfile.write(b'<h1>404 Not Found</h1>')
+
